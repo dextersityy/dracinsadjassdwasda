edb3191ca089a7f4d19baccb5ee1e6ce8028de59
dextersityy <jabalun6@gmail.com>
2026-02-02 21:07:15 +0700

    Update public-api.ts

--- lib/public-api.ts
+++ lib/public-api.ts
@@ -1,6 +1,6 @@
 import { Drama, Episode } from '@/types';
 
-const API_BASE = 'https://dramabox.sansekai.my.id/api/dramabox';
+const API_BASE = 'https://api.sansekai.my.id/api/dramabox';
 
 const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
